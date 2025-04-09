5af07bfa6404df54f6b8e4446fe80ed991197482
bozhidar <br69120586@edu.mon.bg>
2025-04-09 12:11:09 +0300

    imported i18n

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {port: 5174}
-// })
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'https://jets360-backend.vercel.app'
-    }
-  }
-});
+  server: {port: 5174}
+})
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': 'https://jets360-backend.vercel.app'
+//     }
+//   }
+// });
