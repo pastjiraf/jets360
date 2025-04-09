088bf21b344e78447e7647451ddf0c12c56fa5e2
bozhidar <br69120586@edu.mon.bg>
2025-04-09 11:03:45 +0300

    fix admin vite config

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {port: 5174}
+// })
+
 export default defineConfig({
   plugins: [react()],
-  server: {port: 5174}
-})
+  server: {
+    proxy: {
+      '/api': 'https://jets360-backend.vercel.app'
+    }
+  }
+});
